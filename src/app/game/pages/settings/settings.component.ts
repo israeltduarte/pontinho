9390c45b68115260d58f825bbd828d0563d88fc2
Israel Duarte <israeltduarte@gmail.com>
2023-09-07 22:35:49 -0300

    adding configs

--- src/app/game/pages/settings/settings.component.ts
+++ src/app/game/pages/settings/settings.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Player } from 'src/app/core/models';
+import { Player, newEmptyPlayer } from 'src/app/core/models';
+import data from '../../../../api/data.json';
 
 @Component({
   selector: 'settings',
@@ -8,5 +9,41 @@ import { Player } from 'src/app/core/models';
 })
 export class SettingsComponent implements OnInit {
   players!: Player[];
-  ngOnInit(): void {}
+  newPlayer: string = '';
+  points: number = 100;
+  explosions: number = 1;
+
+  ngOnInit(): void {
+    this.players = data.players;
+  }
+
+  addPlayer() {
+    var newPlayer = newEmptyPlayer();
+    newPlayer.name = this.newPlayer;
+    this.players.push(newPlayer);
+    this.clearNewPlayerField();
+  }
+
+  clearNewPlayerField() {
+    this.newPlayer = '';
+  }
+
+  removePlayer(player: Player) {
+    const index = this.players.indexOf(player);
+    if (index !== -1) {
+      this.players.splice(index, 1);
+    }
+  }
+
+  defineMax() {
+    console.log('define new max');
+  }
+
+  defineExplosions() {
+    console.log('define max explosions');
+  }
+
+  saveConfigs() {
+    console.log('save configurations');
+  }
 }
