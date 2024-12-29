f652c8de4f9cb23434ee9f5cca424e0a7bcbfcac
Israel Terra Duarte <israeltduarte@hotmail.com>
2024-12-28 23:22:07 -0300

    test

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+//test
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
